c270db1515df7976ca75816befa6646ea1c86dcc
Piero-HO <piero.huaytalla@tecsup.edu.pe>
2026-05-09 20:00:12 -0500

    agregar vistas y endpoints CRUD

--- students/views.py
+++ students/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from .models import Student, Course
+from .serializers import StudentSerializer, CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # búsqueda
+    filter_backends = [filters.SearchFilter]
+
+    # campos permitidos
+    search_fields = ['name', 'email']
